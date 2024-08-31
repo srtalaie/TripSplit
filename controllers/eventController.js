25bf4276d84e8962a8b7e2ab467cf6f945819cec
srtalaie <srt4721@yahoo.com>
2024-08-31 13:23:40 -0700

    create event route

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -36,10 +36,82 @@ exports.get_all_events = asyncHandler(async (req, res, next) => {
 })
 
 // Get an Event in a Trip
-exports.get_event = asyncHandler(async (req, res, next) => {})
+exports.get_event = asyncHandler(async (req, res, next) => {
+  const decodedToken = jwt.verify(req.token, process.env.SECRET)
+
+  const event = await Event.findById(req.params.event_id)
+    .populate('payee')
+    .populate('payers')
+    .populate('trip')
+    .exec()
+
+  const idCheck = (element) => element.payer.toString() === decodedToken.id
+
+  if (!event) {
+    return res.status(404).send({ message: 'Event not found.' })
+  } else if (decodedToken.id !== event.payee.toString()) {
+    return res.status(401).send({
+      message:
+        'You cannot access this event, you are not the payee of this event.',
+    })
+  } else if (event.payers.length > 0 && !event.payers.some(idCheck)) {
+    return res.status(401).send({
+      message: 'You cannot access this event, you are not part of this event.',
+    })
+  } else {
+    return res.status(200).json(event)
+  }
+})
 
 // Create an Event
-exports.create_event = [asyncHandler(async (req, res, next) => {})]
+exports.create_event = [
+  // Validate and Sanitize user input
+  body('event_name')
+    .trim()
+    .isLength({ min: 3 })
+    .escape()
+    .withMessage('Must provide a first name.'),
+  body('event_description').trim().escape().optional(),
+  body('cost')
+    .trim()
+    .isDecimal()
+    .escape()
+    .withMessage('Must provide a total cost for the event.'),
+
+  // Process request after validation
+  asyncHandler(async (req, res, next) => {
+    // Extract errors from validation
+    const errors = validationResult(req)
+
+    const decodedToken = jwt.verify(req.token, process.env.SECRET)
+
+    if (!decodedToken.email) {
+      return res.status(401).json({ error: 'Token missing or invalid.' })
+    }
+
+    const [user, trip] = await Promise.all([
+      User.findById(decodedToken.id).exec(),
+      Trip.findById(req.params.trip_id).exec(),
+    ])
+
+    const event = new Event({
+      event_name: req.body.event_name,
+      event_description: req.body.event_description,
+      cost: req.body.cost,
+      payee: user._id,
+      trip: trip._id,
+    })
+
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() })
+    } else {
+      const savedEvent = await event.save()
+      const updatedTripEvents = trip.events.concat(savedEvent._id)
+      await Trip.findByIdAndUpdate(trip._id, { events: updatedTripEvents })
+      return res.status(201).json(savedEvent)
+    }
+  }),
+]
 
 // Update an Event
 exports.update_event = [asyncHandler(async (req, res, next) => {})]
--- routes/eventRoutes.js
+++ routes/eventRoutes.js
@@ -4,22 +4,22 @@ const eventController = require('../controllers/eventController')
 
 // Event ROUTES
 
-// Get All Events in a Trip
-router.get('/:trip_id/', eventController.get_all_events)
-
 // Create Event
-router.post('/:trip_id/create', eventController.create_event)
+router.post('/create/:trip_id', eventController.create_event)
 
 // Update Trip
-router.put('/:trip_id/update/:id', eventController.update_event)
+router.put('/update/:trip_id/:event_id', eventController.update_event)
 
 // Update Trip
-router.put('/:trip_id/add_payers/:id', eventController.update_event)
+router.put('/add_payers/:trip_id/:event_id', eventController.update_event)
 
 // Delete Trip
-router.delete('/:trip_id/delete/:id', eventController.delete_event)
+router.delete('/delete/:trip_id/:event_id', eventController.delete_event)
+
+// Get All Events in a Trip
+router.get('/:trip_id', eventController.get_all_events)
 
 // Get An Event in a Trip by :id
-router.get('/:trip_id/:id', eventController.get_event)
+router.get('/:event_id', eventController.get_event)
 
 module.exports = router
