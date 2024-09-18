b73b936c717a9646c64fc8e0f9df3c131bfba621
srtalaie <srt4721@yahoo.com>
2024-09-18 14:15:58 -0700

    created services for frontend

---
+++ client/src/services/eventService.js
@@ -0,0 +1,72 @@
+import axios from 'axios'
+const baseURL = '/events'
+
+let token = null
+
+const get_all_events = async (trip) => {
+  const config = {
+    headers: { Authorization: token },
+  }
+  const req = await axios.get(`${baseURL}/${trip._id}`, config)
+  return req.data
+}
+
+const get_event = async (trip, event) => {
+  const config = {
+    headers: { Authorization: token },
+  }
+  const req = await axios.get(`${baseURL}/${trip._id}/${event._id}`, config)
+  return req.data
+}
+
+const create_event = async (trip, event) => {
+  const config = {
+    headers: { Authorization: token },
+  }
+  const req = await axios.post(`${baseURL}/create/${trip._id}`, event, config)
+  return req.data
+}
+
+const update_event = async (trip, event) => {
+  const config = {
+    headers: { Authorization: token },
+  }
+  const req = await axios.put(
+    `${baseURL}/update/${trip._id}/${event._id}`,
+    event,
+    config
+  )
+  return req.data
+}
+
+const add_payers = async (trip, event, payersArr) => {
+  const config = {
+    headers: { Authorization: token },
+  }
+  const req = await axios.put(
+    `${baseURL}/add_payers/${trip._id}/${event._id}`,
+    payersArr,
+    config
+  )
+  return req.data
+}
+
+const delete_event = async (trip, event) => {
+  const config = {
+    headers: { Authorization: token },
+  }
+  const req = await axios.delete(
+    `${baseURL}/delete/${trip._id}/${event._id}`,
+    config
+  )
+  return req.data
+}
+
+export {
+  add_payers,
+  create_event,
+  delete_event,
+  get_all_events,
+  get_event,
+  update_event,
+}
--- client/src/services/tripService.js
+++ client/src/services/tripService.js
@@ -12,4 +12,47 @@ const get_trip = async (id) => {
   const config = {
     headers: { Authorization: token },
   }
+  const req = await axios.get(`${baseURL}/${id}`, config)
+  return req.data
+}
+
+const create_trip = async (trip) => {
+  const config = {
+    headers: { Authorization: token },
+  }
+  const req = await axios.post(`${baseURL}/create`, trip, config)
+  return req.data
+}
+
+const update_trip = async (trip) => {
+  const config = {
+    headers: { Authorization: token },
+  }
+  const req = await axios.put(`${baseURL}/update/${trip._id}`, trip, config)
+  return req.data
+}
+
+const add_member = async (member) => {
+  const config = {
+    headers: { Authorization: token },
+  }
+  const req = await axios.put(`${baseURL}/add_member/${member._id}`)
+  return req.data
+}
+
+const delete_trip = async (trip) => {
+  const config = {
+    headers: { Authorization: token },
+  }
+  const req = await axios.delete(`${baseURL}/delete/${trip._id}`)
+  return req.data
+}
+
+export {
+  add_member,
+  create_trip,
+  delete_trip,
+  get_all_trips,
+  get_trip,
+  update_trip,
 }
