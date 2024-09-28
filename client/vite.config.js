0dc341825b56f6f262df00a98d25321d15b70e7e
srtalaie <srt4721@yahoo.com>
2024-09-27 17:03:39 -0700

    tailwind setup

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,15 @@
 import react from '@vitejs/plugin-react'
+import tailwindcss from 'tailwindcss'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [tailwindcss()],
+    },
+  },
   server: {
     host: true,
     port: 5174,
