4624a3d22856eebde19990d701d454b74d6ff0b3
srtalaie <srt4721@yahoo.com>
2024-10-16 14:26:31 -0700

    front end

--- client/src/Pages/AddFriendsPage.jsx
+++ client/src/Pages/AddFriendsPage.jsx
@@ -4,10 +4,14 @@ import UserInput from '../components/Input/UserInput'
 
 const AddFriendsPage = () => {
   const [queryText, setQueryText] = useState("")
+  const [queryNumber, setQueryNumber] = useState("")
   const [results, setResults] = useState([])
 
   const users = useSelector((state) => state.users)
 
+
+  //TODO add handleInput for searching by phone number
+  //Add check to see if user is using the phone number field or the name search field
   const handleUserInput = (e) => {
     setQueryText(e.target.value.toLowerCase())
     const filteredUsers = users.filter((user) => {
@@ -30,6 +34,7 @@ const AddFriendsPage = () => {
     <div>
       <h4>Find friends by searching for their name</h4>
       <UserInput type="text" value={queryText} handleChange={handleUserInput} identifier='search' label='Search' />
+      <UserInput type="tel" value={queryNumber} handleChange={handleUserInput} identifier='search' label='Search' />
       {users.length === 0 ? (
         <div>Something went wrong</div>
       ) : (
